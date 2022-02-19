bfee0748efe19e1aaa78b4812ef7ef2523f4bef3
Aleksey <kidai_cuda@mail.ru>
2022-02-19 20:37:43 +0300

    add dir

---
+++ src/baseclasses/response.py
@@ -0,0 +1,31 @@
+from jsonschema import validate
+from src.enums.global_enums import GlobalErrorMessages
+
+
+class Response:
+
+    def __init__(self, response):
+        self.response = response
+        self.response_json = response.json()
+        self.response_status_code = response.status_code
+
+    def validate_json(self, schema):
+        if isinstance(self.response_json, list):
+            for item in self.response_json:
+                validate(item, schema)
+        else:
+            validate(self.response_json, schema)
+
+    def assert_status_code(self, status_code: (int, list)):
+        """
+        Проверка на нужный статус код.
+        :param status_code: ...
+        """
+        if isinstance(status_code, list):
+            assert self.response_status_code in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+        else:
+            assert self.response_status_code == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+
+    def assert_len_json(self, count):
+        assert len(self.response_json) == count, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+
